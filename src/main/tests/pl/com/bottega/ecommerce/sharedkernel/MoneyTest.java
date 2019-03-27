9218231a00ccd3fc7b60f4f74d621ce7d03470f6
Filipowicz.Wiktor <209309@edu.p.lodz.pl>
2019-03-27 18:57:00 +0100

    dodanie testów do Money

---
+++ src/main/tests/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -0,0 +1,59 @@
+package pl.com.bottega.ecommerce.sharedkernel;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public class MoneyTest {
+
+    private Money money;
+    private Money moneyTwo;
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void shouldReturnTrueIfFirstIsGrater() {
+        money = new Money(5);
+        moneyTwo = new Money(0);
+        Assert.assertThat(true, is(money.greaterThan(moneyTwo)));
+    }
+
+    @Test
+    public void shouldReturnFalseIfFirstIsGrater() {
+        money = new Money(5);
+        moneyTwo = new Money(0);
+        Assert.assertThat(false, is(money.lessThan(moneyTwo)));
+    }
+
+    @Test
+    public void shouldReturnTrueIfAreEqual() {
+        money = new Money(5);
+        moneyTwo = new Money(5);
+        Assert.assertThat(true, is(money.lessOrEquals(moneyTwo)));
+    }
+
+    @Test
+    public void shouldReturnTenEuro() {
+        money = new Money(5);
+        moneyTwo = new Money(5);
+        Assert.assertThat("10.00 €", is(money.add(moneyTwo).toString()));
+    }
+
+    @Test
+    public void shouldReturnZeroEuro() {
+        money = new Money(5);
+        moneyTwo = new Money(5);
+        Assert.assertThat("0.00 €", is(money.subtract(moneyTwo).toString()));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionIfCurrenciesAreDifferent() {
+        money = new Money(5, "EUR");
+        moneyTwo = new Money(5, "PL");
+        money.add(moneyTwo);
+    }
+}
